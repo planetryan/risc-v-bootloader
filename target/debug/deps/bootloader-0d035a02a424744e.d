07f97ba3337df5125be11e2fdd607d8fe1bbebbf
planetryan <153133698+ryan-1017@users.noreply.github.com>
2025-05-02 17:15:17 -0400

    updates

---
+++ target/debug/deps/bootloader-0d035a02a424744e.d
@@ -0,0 +1,5 @@
+C:\Users\vboxuser\Documents\risc-v\target\debug\deps\libbootloader-0d035a02a424744e.rmeta: src\main.rs
+
+C:\Users\vboxuser\Documents\risc-v\target\debug\deps\bootloader-0d035a02a424744e.d: src\main.rs
+
+src\main.rs:
