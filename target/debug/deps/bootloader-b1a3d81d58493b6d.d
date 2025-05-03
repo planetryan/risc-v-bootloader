b1cff7fadca222cb15925dcc5d3020b0b210bd86
planetryan <153133698+ryan-1017@users.noreply.github.com>
2025-05-03 15:09:47 -0400

    main.rs

---
+++ target/debug/deps/bootloader-b1a3d81d58493b6d.d
@@ -0,0 +1,5 @@
+C:\Users\vboxuser\Documents\risc-v\target\debug\deps\libbootloader-b1a3d81d58493b6d.rmeta: src\boot_entry.rs
+
+C:\Users\vboxuser\Documents\risc-v\target\debug\deps\bootloader-b1a3d81d58493b6d.d: src\boot_entry.rs
+
+src\boot_entry.rs:
